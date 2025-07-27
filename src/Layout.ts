3e7303c8e317d7391541cf49294ca374228823ee
hydroperx <hydroperfox@gmail.com>
2025-07-27 15:31:12 -0300

    upd

--- src/Layout.ts
+++ src/Layout.ts
@@ -134,13 +134,11 @@ export class LayoutGroup {
     if (this.$.$._dir == "vertical") {
       tiles_width_em =
         this.$.$._group_width*this.$.$._small_size +
-        (this.$.$._group_width-1)*this.$.$._tile_gap +
-        this.$.$._tile_gap*4;
+        (this.$.$._group_width-1)*this.$.$._tile_gap;
     } else {
       tiles_height_em =
         this.$.$._height*this.$.$._small_size +
-        (this.$.$._height-1)*this.$.$._tile_gap +
-        this.$.$._tile_gap*4;
+        (this.$.$._height-1)*this.$.$._tile_gap;
     }
     const to_tween_y_late: { tile: LayoutTile, button: HTMLButtonElement, hEM: number, yEM: number }[] = [];
     for (const [, tile] of this._tiles) {
